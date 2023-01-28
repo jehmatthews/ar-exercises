9597d5106795b0bfd4a0a88c2f08ef1ea6606fb1
jehmatthews <jehmatthews30@gmail.com>
2023-01-28 11:57:55 -0800

    related store and employee classes and created new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,7 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Walter", last_name: "White", hourly_rate: 1000)
+@store2.employees.create(first_name: "Jesse", last_name: "Pinkman", hourly_rate: 1100)
+@store2.employees.create(first_name: "Jordan", last_name: "Matthews", hourly_rate: 25)
